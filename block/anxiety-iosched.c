3ff58bbae2379c945aaffca9baa5829d53db060d
Tyler Nijmeh <tylernij@gmail.com>
2018-09-11 20:28:16 -0700

    block: Introduce Anxiety I/O Scheduler
    The purpose of Anxiety is to improve throughput on latency-sensitive
    tasks on embedded flash storage devices. On modern devices, embedded
    systems contain relatively fast storage, so spending too much time with
    complicated queue-shifting scheduling algorithms may end up
    bottlenecking the device. Similarly to the No-op I/O scheduler, Anxiety
    works with a FIFO queue; however, Anxiety includes basic request logic
    based on READ v. WRITE requests. WRITE requests are starved at a fixed
    ratio, defined as the `max_writes_starved` tunable. READ requests are
    given priority over WRITE requests due to their essentiality in
    determining load-time latency.
    
    ------------------TEST--------------------
    SCHEDULERS	Anxiety		CFQ
    S. READ		286.22		280.74
    S. WRITE	221.01		174.0
    R. READ		41.35		47.89
    R. WRITE	10.65		10.7
    SQL INS.	1276.57		1121.25
    SQL UPD.	2920.97		2978.74
    SQL DEL.	2503.16		2642.3
    ------------------------------------------
    
    Tests were done using AndroBench on a Xiaomi Mi A2 on the latest 4.4.178
    patch. This application has been known to be misrepresentative on I/O
    scores in the past.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>

--- block/anxiety-iosched.c
+++ block/anxiety-iosched.c
@@ -18,42 +18,43 @@
 #include <linux/slab.h>
 #include <linux/init.h>
 
-/* For this many read requests, perform one write request */
-#define	DEFAULT_READ_RATIO	(4)
+/* Max times reads can starve a write */
+#define	DEFAULT_MAX_WRITES_STARVED	(2)
 
 struct anxiety_data {
 	struct list_head queue[2];
-	uint16_t contig_reads;
+	uint16_t writes_starved;
 
 	/* Tunables */
-	uint8_t read_ratio;
+	uint8_t max_writes_starved;
 };
 
-static void anxiety_merged_requests(struct request_queue *q, struct request *rq,
-		struct request *next)
+static void anxiety_merged_requests(struct request_queue *q, struct request *rq, struct request *next)
+
 {
 	rq_fifo_clear(next);
 }
 
 static inline struct request *anxiety_choose_request(struct anxiety_data *adata)
 {
-	/* Perform read requests if ratio is unmet */
-	bool do_read = adata->contig_reads < adata->read_ratio;
+
+	/* Prioritize reads unless writes are exceedingly starved */
+	bool starved = adata->writes_starved > adata->max_writes_starved;
 
 	/* Handle a read request */
-	if (do_read && !list_empty(&adata->queue[READ])) {
-		adata->contig_reads++;
+	if (!starved && !list_empty(&adata->queue[READ])) {
+		adata->writes_starved++;
 		return rq_entry_fifo(adata->queue[READ].next);
 	}
 
 	/* Handle a write request */
 	if (!list_empty(&adata->queue[WRITE])) {
-		adata->contig_reads = 0;
+		adata->writes_starved = 0;
 		return rq_entry_fifo(adata->queue[WRITE].next);
 	}
 
-	/* Completed all pending requests; reset contiguous read counter */
-	adata->contig_reads = 0;
+	/* If there are no requests, then there is nothing to starve */
+	adata->writes_starved = 0;
 	return NULL;
 }
 
@@ -72,14 +73,33 @@ static int anxiety_dispatch(struct request_queue *q, int force)
 
 static void anxiety_add_request(struct request_queue *q, struct request *rq)
 {
-	const uint8_t dir = rq_is_sync(rq);
-	struct anxiety_data *adata = q->elevator->elevator_data;
+	const uint8_t dir = rq_data_dir(rq);
 
-	list_add_tail(&rq->queuelist, &adata->queue[dir]);
+	list_add_tail(&rq->queuelist, &((struct anxiety_data *) q->elevator->elevator_data)->queue[dir]);
 }
 
-static int anxiety_init_queue(struct request_queue *q,
-		struct elevator_type *elv)
+static struct request *anxiety_former_request(struct request_queue *q, struct request *rq)
+{
+	const uint8_t dir = rq_data_dir(rq);
+
+	if (rq->queuelist.prev == &((struct anxiety_data *) q->elevator->elevator_data)->queue[dir])
+		return NULL;
+
+	return list_prev_entry(rq, queuelist);
+}
+
+static struct request *anxiety_latter_request(struct request_queue *q, struct request *rq)
+{
+	const uint8_t dir = rq_data_dir(rq);
+
+	if (rq->queuelist.next == &((struct anxiety_data *) q->elevator->elevator_data)->queue[dir])
+		return NULL;
+
+	return list_next_entry(rq, queuelist);
+}
+
+static int anxiety_init_queue(struct request_queue *q, struct elevator_type *elv)
+
 {
 	struct anxiety_data *adata;
 	struct elevator_queue *eq = elevator_alloc(q, elv);
@@ -100,8 +120,8 @@ static int anxiety_init_queue(struct request_queue *q,
 	/* Initialize */
 	INIT_LIST_HEAD(&adata->queue[READ]);
 	INIT_LIST_HEAD(&adata->queue[WRITE]);
-	adata->contig_reads = 0;
-	adata->read_ratio = DEFAULT_READ_RATIO;
+	adata->writes_starved = 0;
+	adata->max_writes_starved = DEFAULT_MAX_WRITES_STARVED;
 
 	/* Set elevator to Anxiety */
 	spin_lock_irq(q->queue_lock);
@@ -112,20 +132,22 @@ static int anxiety_init_queue(struct request_queue *q,
 }
 
 /* Sysfs access */
-static ssize_t anxiety_read_ratio_show(struct elevator_queue *e, char *page)
+
+static ssize_t anxiety_max_writes_starved_show(struct elevator_queue *e, char *page)
 {
 	struct anxiety_data *adata = e->elevator_data;
 
-	return snprintf(page, PAGE_SIZE, "%u\n", adata->read_ratio);
+	return snprintf(page, PAGE_SIZE, "%u\n", adata->max_writes_starved);
 }
 
-static ssize_t anxiety_read_ratio_store(struct elevator_queue *e,
-		const char *page, size_t count)
+static ssize_t anxiety_max_writes_starved_store(struct elevator_queue *e, const char *page, size_t count)
+
 {
 	struct anxiety_data *adata = e->elevator_data;
 	int ret;
 
-	ret = kstrtou8(page, 0, &adata->read_ratio);
+	ret = kstrtou8(page, 0, &adata->max_writes_starved);
+
 	if (ret < 0)
 		return ret;
 
@@ -133,8 +155,7 @@ static ssize_t anxiety_read_ratio_store(struct elevator_queue *e,
 }
 
 static struct elv_fs_entry anxiety_attrs[] = {
-	__ATTR(read_ratio, 0644, anxiety_read_ratio_show,
-			anxiety_read_ratio_store),
+	__ATTR(max_writes_starved, 0644, anxiety_max_writes_starved_show, anxiety_max_writes_starved_store),
 	__ATTR_NULL
 };
 
@@ -143,8 +164,8 @@ static struct elevator_type elevator_anxiety = {
 		.elevator_merge_req_fn	= anxiety_merged_requests,
 		.elevator_dispatch_fn	= anxiety_dispatch,
 		.elevator_add_req_fn	= anxiety_add_request,
-		.elevator_former_req_fn	= elv_rb_former_request,
-		.elevator_latter_req_fn	= elv_rb_latter_request,
+		.elevator_former_req_fn	= anxiety_former_request,
+		.elevator_latter_req_fn	= anxiety_latter_request,
 		.elevator_init_fn	= anxiety_init_queue,
 	},
 	.elevator_name = "anxiety",
