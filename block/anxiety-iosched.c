da6c41983cc40a91b19216fb82a6ba9c5831c4bd
Tyler Nijmeh <tylernij@gmail.com>
2019-09-11 18:47:33 -0700

    anxiety-iosched: Add low-level dispatch function
    We will use this later.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>
    Signed-off-by: cbendot <r.budhiono@gmail.com>

--- block/anxiety-iosched.c
+++ block/anxiety-iosched.c
@@ -59,6 +59,12 @@ static inline struct request *anxiety_choose_request(struct anxiety_data *adata)
 	return NULL;
 }
 
+static void __anxiety_dispatch(struct request_queue *q, struct request *rq)
+{
+	list_del_init(&rq->queuelist);
+	elv_dispatch_sort(q, rq);
+}
+
 static int anxiety_dispatch(struct request_queue *q, int force)
 {
 	struct request *rq = anxiety_choose_request(q->elevator->elevator_data);
@@ -66,8 +72,7 @@ static int anxiety_dispatch(struct request_queue *q, int force)
 	if (!rq)
 		return 0;
 
-	list_del_init(&rq->queuelist);
-	elv_dispatch_sort(q, rq);
+	__anxiety_dispatch(q, rq);
 
 	return 1;
 }
