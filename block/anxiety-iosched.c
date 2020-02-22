912079c31314faa5bd047b1297e1a8fa5f005f44
Tyler Nijmeh <tylernij@gmail.com>
2020-02-21 16:05:13 -0800

    anxiety-iosched: Remove erroneous dispatch increment
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>
    Signed-off-by: cbendot <r.budhiono@gmail.com>

--- block/anxiety-iosched.c
+++ block/anxiety-iosched.c
@@ -86,8 +86,6 @@ static uint16_t anxiety_dispatch_batch(struct request_queue *q)
 
 			if (!ret)
 				dispatched++;
-
-			dispatched++;
 		}
 
 		/* If we didn't have anything to dispatch; don't batch again */
